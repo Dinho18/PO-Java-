69bd5589091ee1dfbde7cc3ac90fa8987cb0fefd
Dinho <alandersonsousalopes@gmail.com>
2024-03-25 15:02:05 -0300

    add projeto UsandoString

---
+++ UsandoClassesJava/UsandoString.java
@@ -0,0 +1,29 @@
+//Exemplo didáico para uso de string
+// no qual um texto é convertido em maisculo e minusculo
+
+package UsandoClassesJava;
+
+public class UsandoString {
+    public static void main(String[] args) {
+
+        // Entrada
+        String texto; // declarando um objeto de nome texto;
+        texto = new String("joao da silva"); // instanciando o objeto texto
+
+        String endereco = new String("Rua Xingu"); // declara e instancia na mesma linha
+
+        // Processamento
+        String textoMaiusculo = texto.toUpperCase(); // retorna a String em maiuscula e armazena em um novo objeto, sem
+                                                     // alterar o conteudo original
+        String textoMinusculo = texto.toLowerCase(); // retorna a String em minuscula e armazena em um novo objeto, sem
+                                                     // alterar o conteudo original
+
+        endereco = endereco.toUpperCase(); // altera o conteudo original para maiuscula
+
+        // Saida
+        System.out.println(textoMaiusculo); // imprime o objeto na tela
+        System.out.println(textoMinusculo);// imprime o objeto na tela
+        System.out.println(endereco.toLowerCase()); // imprime em letra minuscula sem alterar o conteudo original
+
+    }
+}
